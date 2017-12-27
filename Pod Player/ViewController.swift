3a516a7ed20228499ec336027fc6765958add215
Ariel Rodriguez <ariel@volonbolon.me>
2017-12-27 11:57:41 -0300

    Initial Commit

---
+++ Pod Player/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pod Player
+//
+//  Created by Ariel Rodriguez on 27/12/2017.
+//  Copyright © 2017 Ariel Rodriguez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
